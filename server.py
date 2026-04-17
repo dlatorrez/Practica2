bf7369f31157b1bbe085e1ccb76c5b8a74586e56
dlatorrez <dlatorrez@student.universidadunie.com>
2026-04-17 21:06:51 +0200

    Implementación de tiempo de caducidad de sesión

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
 import os
 from flask import Flask, render_template
 from flask_wtf.csrf import CSRFProtect
+from datetime import timedelta
 
 app = Flask(__name__)
 #app.secret_key = 'supersecretkey'
 app.secret_key = os.environ.get('FLASK_SECRET_KEY', os.urandom(32).hex())
-app.permanent_session_lifetime = 99999999
+app.permanent_session_lifetime = timedelta(minutes=30)
 
 app.config.update(
     SESSION_COOKIE_HTTPONLY=True,
