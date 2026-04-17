3b15f07177947f713f47c87396a9e64e23ebe271
dlatorrez <dlatorrez@student.universidadunie.com>
2026-04-17 23:28:18 +0200

    Segunda remediación de seguridad en instancias de Inyección SQL

--- routes/companies.py
+++ routes/companies.py
@@ -65,8 +65,8 @@ def company_detail(company_id):
     if 'username' not in session:
         return redirect('/login')
     conn = get_data_connection()
-    company = conn.execute("SELECT * FROM companies WHERE id = " + str(company_id)).fetchone()
-    comments = conn.execute("SELECT * FROM comments WHERE company_id = " + str(company_id)).fetchall()
+    company = conn.execute("SELECT * FROM companies WHERE id = ?", (company_id,)).fetchone()
+    comments = conn.execute("SELECT * FROM comments WHERE company_id = ?", (company_id,)).fetchall()
     if request.method == 'POST':
         comment = request.form['comment']
         user = session.get('username')
@@ -114,7 +114,7 @@ def edit_company(company_id):
     if 'username' not in session:
         return redirect('/')
     conn = get_data_connection()
-    company = conn.execute("SELECT * FROM companies WHERE id = "+ str(company_id)).fetchone()
+    company = conn.execute("SELECT * FROM companies WHERE id = ?", (company_id,)).fetchone()
     if not company:
         conn.close()
         return render_template('errors/404.html'), 404
--- routes/companies_admin.py
+++ routes/companies_admin.py
@@ -32,8 +32,8 @@ def delete_company():
         return render_template('errors/403.html'), 403
     company = request.form['company']
     conn = get_data_connection()
-    conn.execute("DELETE FROM companies WHERE id = "+ company)
-    conn.execute("DELETE FROM comments WHERE company_id = " + company)
+    conn.execute("DELETE FROM companies WHERE id = ?", (company,))
+    conn.execute("DELETE FROM comments WHERE company_id = ?", (company,))
     conn.commit()
     conn.close()
     flash("Company deleted.", "warning")
